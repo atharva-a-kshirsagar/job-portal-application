3373ad50a2d36bf8672ac52a766abe7cda018912
sakshi-j-alt <sakshipj02@gmail.com>
2025-05-13 16:21:07 +0530

    first commit

--- job-application/src/main/java/com/capgemini/job_application/JobApplication.java
+++ job-application/src/main/java/com/capgemini/job_application/JobApplication.java
@@ -8,7 +8,7 @@ public class JobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobApplication.class, args);
-		System.out.println(hi hi);
+		System.out.println("hi hi");
 	}
 
 }
